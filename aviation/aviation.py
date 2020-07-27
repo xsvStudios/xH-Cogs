2092da0d3a1aa3396c19f0f2072666d09521d084
AWildTeddyBear <28819241+AWildTeddyBear@users.noreply.github.com>
2020-07-27 18:05:34 -0500

    testing stuff, add airport lookup inital logic

--- aviation/aviation.py
+++ aviation/aviation.py
@@ -1,6 +1,9 @@
 import discord
 from redbot.core import commands
 
+from datetime import datetime
+import json
+
 class Aviation(commands.Cog):
     """
         Has some simple aviation commands such as checking for airport weather, tracking planes, etc...
@@ -9,14 +12,63 @@ class Aviation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def airportLookup(self, airport_code: str, airport_code_type: str):
+        """
+            Loads the airport lookup table from airports.json and loads it into memory.
+            Searches the lookup file for the specefied ICAO or IATA code and returns the hit if found.
+            TODO: Make this more efficient by loading it only once in the __init__ of the cog so we don't waste time loading it again.
+        """
+
+        try:
+            # Open local JSON file
+            with open('airports.json', 'r') as f:
+                airport_obj = json.load(f)
+            
+            # Search the json list for a match
+            # (doing it the long way because that's how the dataset is, and because of the complexity of 2 possible lookups per query)
+            if airport_obj is not None:
+                if len(airport_obj) > 0:
+                    for x in airport_obj:
+                        if airport_code_type == 'ICAO':
+                            # ICAO -> 4 letters
+                            if 'icao' in x:
+                                if x['icao'] == airport_code:
+                                    # Match found, return x
+                                    return x
+                        
+                        elif airport_code_type == 'IATA':
+                            # IATA -> 3 letters
+
+                            if 'iata' in x:
+                                if x['iata'] == airport_code:
+                                    # Match found, return x
+                                    return x
+
+            # Match wasn't found...
+            return None
+        except:
+            # Something blew up?
+            return None
+
+        return None
+
+    def getMetarInfo(self):
+        """
+            TODO: Add description here
+        """
+        # TODO: Add datagrabbing here for the actual metar data. Also include performance metric in returned object.
+
+        return NotImplemented
+
     @commands.command()
     async def metar(self, ctx, station_id: str):
         """
             Gets the current weather information from a specified airport IATA or ICAO airport code.
 
             General list of airports with IATA and ICAO codes: https://en.wikipedia.org/wiki/Lists_of_airports
-            IATA Codes (3 letters): https://en.wikipedia.org/wiki/IATA_airport_code
-            ICAO Codes (4 letters): https://en.wikipedia.org/wiki/ICAO_airport_code
+            **ICAO** Codes (4 letters): https://en.wikipedia.org/wiki/ICAO_airport_code
+            **IATA** Codes (3 letters): https://en.wikipedia.org/wiki/IATA_airport_code
+            
             Will return the data in an embed to the user with useful weather information of the airport.
 
             **Example**:
@@ -30,7 +82,65 @@ class Aviation(commands.Cog):
             ```
         """
 
+        # Checks to see if the input string is a ICAO or IATA input
+        if (not station_id.isalpha()) or (len(station_id) != 3) or (len(station_id) != 4):
+            return await ctx.send(f'Your input must be an ICAO or IATA code. Get more info by typing: {ctx.prefix}help {ctx.command}')
+
+        # TODO: Do lookup table here and get information on it such as airport name, etc...
+
+        # TODO: Get data here and store it in an dictionary and perform datachecks on it
+
+        """
+            Get data from respective code type.
+        """
+
+        # Define global object to assign in the case types below
+        station_obj = None
+
+        if len(station_id) == 4:
+            # ICAO Code handling
+
+            # Lookup ICAO code and return object if found
+            icao_lookup = airportLookup(station_id, 'ICAO')
+            if icao_lookup is None:
+                return await ctx.send(f'Error: The airport ICAO code with input ID \'{station_id}\' was not found!')
+
+            # Lookup was successful
+            station_obj = icao_lookup
+        elif len(station_id) == 3:
+            # IATA Code handling
+
+            # Lookup IATA code and return object if found
+            iata_lookup = airportLookup(station_id, 'IATA')
+            if iata_lookup is None:
+                return await ctx.send(f'Error: The airport IATA code with input ID \'{station_id}\' was not found!')
+
+            # Lookup was successful
+            station_obj = iata_lookup
+        else:
+            # If this gets hit, then it somehow avoided the filters? How???
+            return await ctx.send(f'Your input must be an ICAO or IATA code. Get more info by typing: {ctx.prefix}help {ctx.command}')
+
+        # Exit if something bad happened. (over-engineering, but whatever)
+        if station_obj is None:
+            return await ctx.send('An error occured looking up your airport code. Please try again or try a different code.')
+
+        # temp
+        return await ctx.send(station_obj)
+
+        # Construct embed
         try:
-            return await ctx.send('This is a test message to make sure the cog is working properly.')
-        except Exception as e:
-            return await ctx.send(f'Fatal exception in metar command: {e}')
+            embed = discord.Embed(
+                title=f'',
+                description='',
+                color=0x8b0eeb,
+            )
+
+            # Set UTC date on timestamp so discord can parse it
+            embed.timestamp(datetime.utcnow())
+
+            # Send embed
+            return await ctx.send(embed=embed)
+
+        except:
+            return None
