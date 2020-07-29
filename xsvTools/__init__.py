18fa73ab03317dfba84fbc6b6ad31dd24e6f3de4
xblynd <blynd.xsv@outlook.com>
2020-07-29 18:46:55 -0400

    moving to github for Doc Control for Cuddles

---
+++ xsvTools/__init__.py
@@ -0,0 +1,11 @@
+{
+    "author": ["blynd"],
+    "install_msg": "These tools are specifically designed for xsvCommunity's discord.  Some commands are authored by non-members.  Credits per in readme.",
+    "name": "xsvCommunity",
+    "disabled": false,
+    "short": "",
+    "description": "Some cogs that are specifically for the community.",
+    "tags": [],
+    "requirements": [],
+    "hidden": false
+}
