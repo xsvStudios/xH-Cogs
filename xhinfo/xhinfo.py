564d3c25786b0dd6b4f9e8ffd37ad3853c479c2a
xBlynd <blynd.xsv@outlook.com>
2020-08-08 14:27:43 -0400

    format change

--- xhinfo/xhinfo.py
+++ xhinfo/xhinfo.py
@@ -29,59 +29,27 @@ class Information(commands.Cog):
         """
             Display rules for xhCommunity
         """
-        msg = """**Moderator Guide**
-
-```css
-< General >
-**Keep Shit calm**
-- Main task.. thats it...
-**Mediation** (if required)
-- May be requested or required to contain toxicity from spewing.  Case by case basis
-
-```css
-< Public Channels >
-```
-**Personal Attacks**
-- Moderator to interject and drop the gavel on the discussion.
-- Personal attack posts to be removed.
-- If required the entire conversation to be removed.
-
-**Political Based Posts** *(within means)*
-- Keep it in the Politics Channels
-- Remove any posts that are outside of the channel.
-- Personal attacks removed.
-
-**Disputes**
-- Interject the situation and STOP it.
-- If Members tell them, move it to members channel or private.
-- If not-members tell, then stfu nicely and move to private.
-- Remove posts is deemed necessary.
-
-**NSFW**
-- Remove posts from public channels that may be classified as NSFW. *Moderator discretion*
-
-```css
-< Members Channels >
-```
-**Personal Attacks**
-- Interject as quickly as possible and stop the discussion
-- If the members that were involved went overboard or stepped out of line. Moderator can add a warning for future review on the instance a member ever gets put in for a member removal vote."""
+        msg = """
+        
+        
+        """
         await ctx.send(msg)
 
     @commands.command()
-    async def test(self, ctx):
+    async def modguide(self, ctx):
         text = """**Moderator Guide**
 
-```css
-< General >
+```ini
+[ General Duties ]
 ```
 **Keep Shit calm**
 - Main task.. thats it...
+
 **Mediation** (if required)
 - May be requested or required to contain toxicity from spewing.  Case by case basis
 
-```css
-< Public Channels >
+```ini
+[ Public Channels Practices ]
 ```
 **Personal Attacks**
 - Moderator to interject and drop the gavel on the discussion.
@@ -102,8 +70,8 @@ class Information(commands.Cog):
 **NSFW**
 - Remove posts from public channels that may be classified as NSFW. *Moderator discretion*
 
-```css
-< Members Channels >
+```ini
+[ Members Channels Practices ]
 ```
 **Personal Attacks**
 - Interject as quickly as possible and stop the discussion
