35001e6fdc59b69ff0f4a682f03063b6fb1d53ce
xBlynd <blynd.xsv@outlook.com>
2020-08-08 14:15:37 -0400

    test

--- xhinfo/xhinfo.py
+++ xhinfo/xhinfo.py
@@ -70,43 +70,7 @@ class Information(commands.Cog):
 
 @commands.command()
 async def test(self, ctx):
-    text = """**Moderator Guide**
-
-```css
-< General >
-**Keep Shit calm**
-- Main task.. thats it...
-**Mediation** (if required)
-- May be requested or required to contain toxicity from spewing.  Case by case basis
-
-```css
-< Public Channels >
-```
-**Personal Attacks**
-- Moderator to interject and drop the gavel on the discussion.
-- Personal attack posts to be removed.
-- If required the entire conversation to be removed.
-
-**Political Based Posts** *(within means)*
-- Keep it in the Politics Channels
-- Remove any posts that are outside of the channel.
-- Personal attacks removed.
-
-**Disputes**
-- Interject the situation and STOP it.
-- If Members tell them, move it to members channel or private.
-- If not-members tell, then stfu nicely and move to private.
-- Remove posts is deemed necessary.
-
-**NSFW**
-- Remove posts from public channels that may be classified as NSFW. *Moderator discretion*
-
-```css
-< Members Channels >
-```
-**Personal Attacks**
-- Interject as quickly as possible and stop the discussion
-- If the members that were involved went overboard or stepped out of line. Moderator can add a warning for future review on the instance a member ever gets put in for a member removal vote."""
+    text = "Hello World"
     await ctx.maybe_send_embed(simple_text)
 
     @commands.command()
