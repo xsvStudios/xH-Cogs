26f2551d656b15e226f5ae5a0840503fd6244311
xBlynd <blynd.xsv@outlook.com>
2020-08-08 15:59:00 -0400

    rules updated

--- xhinfo/xhinfo.py
+++ xhinfo/xhinfo.py
@@ -179,6 +179,32 @@ After your **6** month period your application for **Member** tags will go to a 
 
 
     @commands.command()
+    async def ourrules(self, ctx):
+        text = """
+        **Community Rules**
+*We give the benefit of the doubt...  Don't be a dumbass and you'll be fine.
+
+**Don't be a dick..**
+People can be dumb, we know this. We will deal with the problem swiftly. Do not antagonize.
+**Act like an adult…**
+You are NOT more important than your neighbor…  Community is 18+
+**Racism is ZERO TOLERANCE**
+Period.  You want your opinion, cool.. Keep it to PM’s and out of public discord.
+**Harassment is ZERO TOLERANCE**
+There is a difference between talking shit as friends and attacking someone… you should know it. If you don’t, we will help make it clear.
+**Excessive Bashing is ZERO TOLERANCE**
+Whether it's our group, or another community - don't bash them or their staff. Not cool and we do not support it.
+**Trolling**
+If you're making off-topic comments, taking threads off-topic, or posting random irrelevant material, you're likely to find a boot in your ass.
+**NSFW Material**
+Not allowed in public areas of discord, sorry. 
+We do have a members only NSFW channel.  This is NOT public by design.  
+**Community Promotions**
+The community has approved partner communities. If they are not on the approved list don't share it. Partner list will be available with command !partners very soon.
+        """
+        await ctx.maybe_send_embed(text)
+
+    @commands.command()
     async def donate(self, ctx: commands.Context):
         """
             Become a Community Donor for perks!
@@ -190,3 +216,5 @@ After your **6** month period your application for **Member** tags will go to a 
             "VPN access, 0365, 1TB Cloud space, and more :smile:"
         )
         await ctx.send(msg)
+
+
