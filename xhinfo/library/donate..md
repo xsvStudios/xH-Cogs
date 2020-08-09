33d61d00f5c89005cd8104ea5cda470db4335d51
xBlynd <blynd.xsv@outlook.com>
2020-08-09 17:43:38 -0400

    test

--- xhinfo/library/donate..md
+++ xhinfo/library/donate..md
@@ -9,7 +9,22 @@
     - 0365
 
 
-    **Community Donations**
-*Donations are a very overlooked issue.  Most poeple don't look into how this is used.  We disagree with this concept.  All money should be accountable.  
+**Community Donations**
+Community Donations are appreciated but **not** required!  Occasionally we will self host gameservers that have a operating cost.  To maintain these servers we utilize the community donations when nessacary.
+
+__Donations Policy__
+Do you host a server under the community name?  Sweet ! We know that most of the cost comes out of your own wallet!  We don't want to control that.  However the moment you list for donations we require a few things to make sure our community name is not abused.  All donations must be reported to the Council monthy.  We only ask that all funds recieved in the form of donations be utilized specifically for its purpose.  Any donations that exceeds your requirements must be then put into the Community Donations Fund.  
+
+__Accountability__
+Donations are a very overlooked item.  Most poeple don't look into how this is used.  We disagree with this concept as we have witnessed members stealing donations for personal gain.  All money should in which anyone donates to group is required to be 100% accounted for.  
 The one thing we strive to do is make sure our books are open.  All donations recieved are added to a community fund controlled by the **Council**. 
-Any withdrawl from donations requires community approval.  Community votes are hosted once a month on dates specified in the **Community Meeting Board**
+Any withdrawl from donations requires community approval.  Community meetings are hosted once a month to review the accounts as to eliminate any abuse.
+
+__Donor Perks__
+We offer some sweet ass perks from monthly donors for a little as $5 a month. 
+- Private Discord Channel Access
+- Whitelisted and VIP status on all gameservers- 
+- Office 365
+- VPN's
+- Cloud Services
+- Streaming Services
--- xhinfo/xhinfo.py
+++ xhinfo/xhinfo.py
@@ -230,10 +230,25 @@ The community has approved partner communities. If they are not on the approved 
         Our community Rules
         """
         text = """
-    **Community Donations**
-*Donations are a very overlooked issue.  Most poeple don't look into how this is used.  We disagree with this concept.  All money should be accountable.  
-The one thing we strive to do is make sure our books are open.  All donations recieved are added to a community fund controlled by the **Council**. 
-Any withdrawl from donations requires community approval.  Community votes are hosted once a month on dates specified in the **Community Meeting Board**
 
+**Community Donations**
+Community Donations are appreciated but **not** required!  Occasionally we will self host gameservers that have a operating cost.  To maintain these servers we utilize the community donations when necessary.
+
+__Donations Policy__
+Do you host a server under the community name?  Sweet ! We know that most of the cost comes out of your own wallet!  We don't want to control that.  However the moment you list for donations we require a few things to make sure our community name is not abused.  All donations must be reported to the Council monthly.  We only ask that all funds received in the form of donations be utilized specifically for its purpose.  Any donations that exceeds your requirements must be then put into the Community Donations Fund.  
+
+__Accountability__
+Donations are a very overlooked item.  Most people don't look into how this is used.  We disagree with this concept as we have witnessed members stealing donations for personal gain.  All money in which anyone donates to group is required to be 100% accounted for.  
+The one thing we strive to do is make sure our books are open.  All donations received are added to a community fund controlled by the **Council**.   Any withdrawal from donations requires community approval.  Community meetings are hosted once a month to review the accounts as to eliminate any abuse.
+
+__Donor Perks__
+We offer some sweet ass perks for monthly donors for a little as $5 a month.  (This basically works off family plan accounts.)
+- Private Discord Channel Access
+- Whitelisted and VIP status on all gameservers
+- Office 365
+- VPN's
+- Cloud Services
+- Streaming Services
+- Flex Streaming 1k+ Movies and TV Shows (currently relocating)
         """
         await ctx.maybe_send_embed(text)
