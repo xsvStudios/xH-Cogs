62b1fdb222c9101a0f93ebd949d5d608875de9ac
xBlynd <blynd.xsv@outlook.com>
2020-09-03 01:46:18 -0400

    Done?

--- volunteer/volunteer.py
+++ volunteer/volunteer.py
@@ -42,7 +42,7 @@ class Volunteer(commands.Cog):
         except TypeError:
             role_add = None
         if not role_add:
-            role_add = get(ctx.guild.roles, name = "Staff Applicant")
+            role_add = get(ctx.guild.roles, name = "Volunteering")
             if not role_add:
                 return await ctx.send("Uh oh, the configuration is not correct. Ask the Admins to set it.")
         try:
@@ -50,7 +50,7 @@ class Volunteer(commands.Cog):
         except TypeError:
             channel = None
         if not channel:
-            channel = get(ctx.guild.text_channels, name = "staff applications")
+            channel = get(ctx.guild.text_channels, name = "volunteer applications")
             if not channel:
                 return await ctx.send("Uh oh, the configuration is not correct. Ask the Admins to set it.")
         if ctx.guild not in self.antispam:
@@ -70,26 +70,30 @@ class Volunteer(commands.Cog):
         try:
             await ctx.author.send(
                 """
-**Looking to join one the xH Community Support Teams?**\n
-We are currently accepting volunteers applications for the roles listed below:
+**Looking to join one of the xH Community Support Teams?**
+The roles listed below are currently looking for volunteers.  If you are interested in joining one of our teams, we have a small application for you.
 
 **Moderator Team:** Volunteers that help monitor and maintain the communities rules and values in our discord.
-**Development Team:** A group of hobbyist and professionals that assist in gameserver creation, server management, game modding, website development, support the Community Outreach Team, etc.
-**Design Team:** They make the community look awesome...  The creative members of the community that have talent in the design world and help with logos, website design, gameserver design, support the Community Outreach Team, etc..
-**Community Outreach:** This group contains the approved community streamers, social media platform team, community youtube content creators, coordinates community Events, giveaways and raffles. 
-~~**Game Server Admins** - Volunteers to assist in our active game servers.~~ *Check back later.
 
-Applications will be reviewed by the **xH Community Council**.  A update from the team will be sent to you no longer then 7 days after app is submitted.
+**Development Team:** A group of hobbyist and professionals that assist in gameserver creation, server management, game modding, website development, support the Community Outreach Team, etc.
+
+**Design Team:** They make the community look awesome...  The creative members of the community that have talent in the design world and help with logos, website design, gameserver design, support the Community Outreach Team, etc..
+
+**Community Outreach:** This group contains the approved community streamers, social media platform team, community youtube content creators, coordinates community Events, giveaways and raffles. 
+
+~~**Game Server Admins** - Volunteers to assist in our active game servers.~~ *Coming soon! Check back later.*
+
+Applications will be reviewed by the `xH Community Council`.  A update from the team will be sent to you no longer then 7 days after app is submitted.
 
 ***What position are you applying for?***
-*You have maximum of 5 minutes for each following question.*                
+*You have maximum of 5 minutes for each following question.*              
                 """
             )
         except discord.Forbidden:
             return await ctx.send(
                 "I don't seem to be able to DM you. Do you have DM's turned off?"
             )
-        await ctx.send(f"> Okay, {ctx.author.mention}, i've sent your a DM.")
+        await ctx.send(f"Okay, {ctx.author.mention}, i've sent your a DM.")
 
         def check(m):
             return m.author == ctx.author and m.channel == ctx.author.dm_channel
@@ -98,7 +102,7 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
             position = await self.bot.wait_for("message", timeout=30, check=check)
         except asyncio.TimeoutError:
             return await ctx.author.send("```diff\n- You took too long. Try again, please.```")
-        await ctx.author.send("> What is your name?")
+        await ctx.author.send("What is your name?")
         try:
             name = await self.bot.wait_for("message", timeout=30, check=check)
         except asyncio.TimeoutError:
@@ -107,36 +111,36 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
         try:
             age = await self.bot.wait_for("message", timeout=30, check=check)
         except asyncio.TimeoutError:
-            return await ctx.send("You took too long. Try again, please.")
+            return await ctx.author.send("```diff\n- You took too long. Try again, please.```")
         await ctx.author.send("What timezone are you in? (Google is your friend.)")
         try:
             timezone = await self.bot.wait_for("message", timeout=30, check=check)
         except asyncio.TimeoutError:
-            return await ctx.send("You took too long. Try again, please.")
+            return await ctx.author.send("```diff\n- You took too long. Try again, please.```")
         await ctx.author.send("How many days per week can you be active?")
         try:
             days = await self.bot.wait_for("message", timeout=30, check=check)
         except asyncio.TimeoutError:
-            return await ctx.send("You took too long. Try again, please.")
+            return await ctx.author.send("```diff\n- You took too long. Try again, please.```")
         await ctx.author.send("How many hours per day can you be active?")
         try:
             hours = await self.bot.wait_for("message", timeout=30, check=check)
         except asyncio.TimeoutError:
-            return await ctx.send("You took too long. Try again, please.")
+            return await ctx.author.send("```diff\n- You took too long. Try again, please.```")
         await ctx.author.send(
             "Do you have any previous experience? If so, please describe."
         )
         try:
             experience = await self.bot.wait_for("message", timeout=30, check=check)
         except asyncio.TimeoutError:
-            return await ctx.send("You took too long. Try again, please.")
+            return await ctx.author.send("```diff\n- You took too long. Try again, please.```")
         await ctx.author.send("Why do you want to be a member of our staff?")
         try:
             reason = await self.bot.wait_for("message", timeout=30, check=check)
         except asyncio.TimeoutError:
-            return await ctx.send("You took too long. Try again, please.")
+            return await ctx.author.send("```diff\n- You took too long. Try again, please.```")
         embed = discord.Embed(color=await ctx.embed_colour(), timestamp=datetime.now())
-        embed.set_author(name="New application!", icon_url=ctx.author.avatar_url)
+        embed.set_author(name="Volunteer Application!", icon_url=ctx.author.avatar_url)
         embed.set_footer(
             text=f"{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})"
         )
@@ -159,7 +163,7 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
         await ctx.author.add_roles(role_add)
 
         await ctx.author.send(
-            "Your application has been sent to the Admins, thank you!"
+            "Thank you, I have sent your application to the team.  As mentioned above I will provide you an update in no later the 7 days."
         )
         self.antispam[ctx.guild][ctx.author].stamp()
 
@@ -172,8 +176,8 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
         pred = MessagePredicate.yes_or_no(ctx)
         role = MessagePredicate.valid_role(ctx)
 
-        applicant = get(ctx.guild.roles, name="Staff Applicant")
-        channel = get(ctx.guild.text_channels, name="staff applications")
+        applicant = get(ctx.guild.roles, name="Volunteer Applicant")
+        channel = get(ctx.guild.text_channels, name="volunteer applications")
 
         await ctx.send(
             "This will create required channel and role. Do you wish to continue? (yes/no)"
@@ -187,7 +191,7 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
         if not applicant:
             try:
                 applicant = await ctx.guild.create_role(
-                    name="Staff Applicant", reason="Application cog setup"
+                    name="Volunteering", reason="Application cog setup"
                 )
             except discord.Forbidden:
                 return await ctx.send(
@@ -217,9 +221,9 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
                 }
             try:
                 channel = await ctx.guild.create_text_channel(
-                    "applications",
+                    "volunteer applications",
                     overwrites=overwrites,
-                    reason="Application cog setup",
+                    reason="Volunteer cog setup",
                 )
             except discord.Forbidden:
                 return await ctx.send(
@@ -242,7 +246,7 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
     @commands.guild_only()
     @checks.bot_has_permissions(manage_roles=True)
     async def volunteeraccept(self, ctx: commands.Context, target: discord.Member):
-        """Accept a staff applicant.
+        """Accept a volunteer.
 
         <target> can be a mention or an ID."""
         try:
@@ -260,7 +264,7 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
         except TypeError:
             applicant = None
         if not applicant:
-            applicant = get(ctx.guild.roles, name="Staff Applicant")
+            applicant = get(ctx.guild.roles, name="Volunteering")
             if not applicant:
                 return await ctx.send("Uh oh, the configuration is not correct. Ask the Admins to set it.")
         role = MessagePredicate.valid_role(ctx)
@@ -278,7 +282,7 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
             await target.remove_roles(applicant)
             await ctx.send(f"Accepted {target.mention} as {role_add}.")
             await target.send(
-                f"You have been accepted as {role_add} in {ctx.guild.name}."
+                f"You have been accepted as {role_add} in {ctx.guild.name}.  A member of the **xH Council** team will be with you shortly for a small briefing."
             )
         else:
             await ctx.send(
@@ -289,7 +293,7 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
     @commands.guild_only()
     @checks.bot_has_permissions(manage_roles=True)
     async def volunteerdeny(self, ctx: commands.Context, target: discord.Member):
-        """Deny a staff applicant.
+        """Deny a volunteer.
 
         <target> can be a mention or an ID"""
         try:
@@ -307,7 +311,7 @@ Applications will be reviewed by the **xH Community Council**.  A update from th
         except TypeError:
             applicant = None
         if not applicant:
-            applicant = get(ctx.guild.roles, name="Staff Applicant")
+            applicant = get(ctx.guild.roles, name="Volunteering")
             if not applicant:
                 return await ctx.send("Uh oh, the configuration is not correct. Ask the Admins to set it.")
         if applicant in target.roles:
