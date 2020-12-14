85f65ec33883d6c23f9df5f543a94df83eeb07d4
xBlynd <blynd.xsv@outlook.com>
2020-12-14 00:54:00 -0500

    changed some things

--- xhinfo/xhinfo.py
+++ xhinfo/xhinfo.py
@@ -183,45 +183,45 @@ After your **6** month period your application for **Member** tags will go to a 
         await ctx.maybe_send_embed(text)
 
 
-    @commands.command()
-    async def rules(self, ctx):
-        """
-        Our community Rules
-        """
-        text = """
-**Community Rules**
-*We give the benefit of the doubt...  Don't be a dumbass and you'll be fine.*
+#     @commands.command()
+#     async def rules(self, ctx):
+#         """
+#         Our community Rules
+#         """
+#         text = """
+# **Community Rules**
+# *We give the benefit of the doubt...  Don't be a dumbass and you'll be fine.*
 
-__**Don't be a dick..**__
-People can be dumb, we know this. We will deal with the problem swiftly. Do not antagonize.
-__**Act like an adult…**__
-You are NOT more important than your neighbor…  Community is 18+
-__**Racism is ZERO TOLERANCE**__
-Period.  You want your opinion, cool.. Keep it to PM’s and out of public discord.
-__**Harassment is ZERO TOLERANCE**__
-There is a difference between talking shit as friends and attacking someone… you should know it. If you don’t, we will help make it clear.
-__**Excessive Bashing is ZERO TOLERANCE**__
-Whether it's our group, or another community - don't bash them or their staff. Not cool and we do not support it.
-__**Trolling**__
-If you're making off-topic comments, taking threads off-topic, or posting random irrelevant material, you're likely to find a boot in your ass.
-__**NSFW Material**__
-Not allowed in public areas of discord, sorry. 
-We do have a members only NSFW channel.  This is NOT public by design.  
-__**Community Promotions**__
-The community has approved partner communities. If they are not on the approved list don't share it. Partner list will be available with command !partners very soon.
-        """
-        await ctx.maybe_send_embed(text)
+# __**Don't be a dick..**__
+# People can be dumb, we know this. We will deal with the problem swiftly. Do not antagonize.
+# __**Act like an adult…**__
+# You are NOT more important than your neighbor…  Community is 18+
+# __**Racism is ZERO TOLERANCE**__
+# Period.  You want your opinion, cool.. Keep it to PM’s and out of public discord.
+# __**Harassment is ZERO TOLERANCE**__
+# There is a difference between talking shit as friends and attacking someone… you should know it. If you don’t, we will help make it clear.
+# __**Excessive Bashing is ZERO TOLERANCE**__
+# Whether it's our group, or another community - don't bash them or their staff. Not cool and we do not support it.
+# __**Trolling**__
+# If you're making off-topic comments, taking threads off-topic, or posting random irrelevant material, you're likely to find a boot in your ass.
+# __**NSFW Material**__
+# Not allowed in public areas of discord, sorry. 
+# We do have a members only NSFW channel.  This is NOT public by design.  
+# __**Community Promotions**__
+# The community has approved partner communities. If they are not on the approved list don't share it. Partner list will be available with command !partners very soon.
+#         """
+#         await ctx.maybe_send_embed(text)
 
     @commands.command()
     async def donate(self, ctx: commands.Context):
         """
             Become a Community Donor for perks!
-            https://xsvcommunity.com/donate
+            https://www.patreon.com/xhcommunity
         """
         msg = (
-            "Help support the Communities growth."
-            "Donor's get a longlist of perks ranging from Streaming services, "
-            "VPN access, 0365, 1TB Cloud space, and more :smile:"
+            "Help support the communities gameservers and bs operations costs."
+            "Find out more at the Monthly Community Donor Meeting"
+            
         )
         await ctx.send(msg)
 
