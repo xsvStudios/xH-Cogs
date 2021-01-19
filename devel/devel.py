1b61a6232489ef09f9403501fcb27cc22f4c23af
xBlynd <blynd.xsv@outlook.com>
2021-01-18 23:06:46 -0500

    idk?

--- devel/devel.py
+++ devel/devel.py
@@ -5,7 +5,10 @@ class Devel(commands.Cog):
     This is a random development from Blynd.
     
     """
+      def __init__(self, bot):
+        self.bot = bot
 
+    
     @commands.command()
     async def testy(self, ctx):
         text = "Hello World"
