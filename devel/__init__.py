9cb056d53070b6e7453514d4559083da03c8444e
xBlynd <blynd.xsv@outlook.com>
2021-01-18 23:14:09 -0500

    okay?

--- devel/__init__.py
+++ devel/__init__.py
@@ -1,5 +1,5 @@
 from .devel import Devel
 
-# Setup file to read in the cog
+
 def setup(bot):
-    bot.add_cog(Devel(bot))
+    bot.add_cog(Devel())
--- devel/devel.py
+++ devel/devel.py
@@ -1,10 +1,10 @@
 from redbot.core import commands
 
-class Devel(commands.Cog):
-    """Testing over"""
+class Mycog(commands.Cog):
+    """My custom cog"""
 
     @commands.command()
     async def testy(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can break things thats for sure!")
+        await ctx.send("I can do stuff!")
