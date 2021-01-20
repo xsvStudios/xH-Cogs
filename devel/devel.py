47a835087a7060957fea009aed582efd0a4530d8
xBlynd <blynd.xsv@outlook.com>
2021-01-20 02:46:40 -0500

    easier then i think?

--- devel/devel.py
+++ devel/devel.py
@@ -58,4 +58,4 @@ class Devel(commands.Cog):
         embed.add_field(name='Are you setting up for Member Apps?', value='Hit the ***')
         embed.set_footer(text='Brought to you by xH Development team')
 
-        await ctx.send(opener_em=embed)
+        await ctx.send(opener_em)
