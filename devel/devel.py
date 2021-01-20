0b8294d078ca0a2e48b29abaa7884e3f6dadccaf
xBlynd <blynd.xsv@outlook.com>
2021-01-20 03:11:26 -0500

    menu and controls added

--- devel/devel.py
+++ devel/devel.py
@@ -3,6 +3,7 @@ import discord
 
 from discord.utils import get
 from redbot.core import Config, checks, commands
+from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 
 from redbot.core.bot import Red
 from .xsvCore import *
