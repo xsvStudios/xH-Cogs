d8a39d834b3550cb4940711d3e2d23cdb418eaa8
xBlynd <blynd.xsv@outlook.com>
2021-01-23 18:44:50 -0500

    should?

--- worklist/worklist.py
+++ worklist/worklist.py
@@ -69,6 +69,7 @@ class Worklist(commands.Cog):
         """
         guild =  ctx.message.guild
         worklists_test = await self.database.guild(ctx.guild).Worklist()
+        msg = "This should give me a list?"
         for m in worklists_test:
             msg += "  {}. {}\n".format(c)
         for page in pagify(msg, ["\n", " "], shorten_by=20):
